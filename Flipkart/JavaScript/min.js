74aaac789379271a7e7137a4b0d83a0706831099
Disha-56843 <113577480+Disha-56843@users.noreply.github.com>
2023-06-27 12:19:02 +0530

    Add files via upload

---
+++ Flipkart/JavaScript/min.js
@@ -0,0 +1,14 @@
+function contact(){
+    window.location="file:///d%3A/17jun_530TTS/Task/Flipkart/page1.html";
+}
+function login(){
+    window.location="file:///d%3A/17jun_530TTS/Task/Flipkart/page3.html";
+}
+function cart(){
+    window.location="file:///d%3A/17jun_530TTS/Task/Flipkart/page7.html";
+
+}
+function home(){
+    window.location="file:///d%3A/17jun_530TTS/Task/Flipkart/index.html";
+
+}
